4f6618ef022678f5e8dad3847d5c838637191345
Riccardo Pucella <riccardo.pucella@gmail.com>
2025-06-01 00:22:11 -0400

    Remove REPL from engine definition

--- cmd/gcl/main.go
+++ cmd/gcl/main.go
@@ -2,11 +2,94 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"strings"
+	"bufio"
+	"os"
 	"rpucella.net/go-cl/gcl"
 )
 
 func main() {
-	fmt.Println("Go Command Language Standalone Interpreter 1.0.0")
+	fmt.Println("Go Command Language Standalone Interpreter 1.0.0.")
 	eng := gcl.NewEngine()
-	eng.Repl("gcl")
+	eng.AddPrimitive("quit", 0, 0, primitiveQuit)
+	repl(eng)
+}
+
+func repl(eng gcl.Engine) {
+	prompt := "gcl"
+	reader := bufio.NewReader(os.Stdin)
+	direct := false
+	fmt.Println("Direct mode: start line with /, toggle with isolated /.")
+
+	for {
+		tempDirect := false
+		if direct {
+			fmt.Printf("%s(direct)> ", prompt)
+		} else {
+			fmt.Printf("%s> ", prompt)
+		}
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println()
+				bail()
+			}
+			fmt.Println("IO ERROR - ", err.Error())
+		}
+		text = strings.TrimSpace(text)
+		if text == "" {
+			continue
+		}
+		if text[0] == '/' {
+			if strings.TrimSpace(text[1:]) == "" {
+				direct = !direct
+				continue
+			} else {
+				text = text[1:]
+				tempDirect = true
+			}
+		}
+		var sexp gcl.Value
+		if direct || tempDirect {
+			sexp, err = eng.ReadList(text)
+			if err != nil {
+				fmt.Println("READ ERROR -", err.Error())
+				continue
+			}
+		} else {
+			sexp, err = eng.Read(text, "")
+			if err != nil {
+				fmt.Println("READ ERROR -", err.Error())
+				continue
+			}
+		}
+		name, err := eng.ProcessDeclaration(sexp)
+		if err != nil {
+			fmt.Println("PARSE ERROR -", err.Error())
+			continue
+		}
+		if name != "" {
+			fmt.Println(name)
+			continue
+		}
+		v, err := eng.Eval(sexp)
+		if err != nil {
+			fmt.Println(err.Error)
+			continue
+		}
+		// need to export isNil!
+//		if !v.IsNil() {
+			fmt.Println(v.Display())
+//		}
+	}
+}
+
+func bail() {
+	os.Exit(0)
+}
+
+func primitiveQuit(name string, args []gcl.Value) (gcl.Value, error) {
+	bail()
+	return gcl.NewNil(), nil
 }
--- gcl/engine.go
+++ gcl/engine.go
@@ -1,10 +1,8 @@
 package gcl
 
-import "fmt"
-import "bufio"
-import "os"
-import "strings"
-import "io"
+import (
+	"fmt"
+)
 
 type Engine struct {
 	env *Env
@@ -18,6 +16,7 @@ func NewEngine() Engine {
 	return Engine{env}
 }
 
+
 // TODO: engine.Read()
 // TODO: engine.Eval()
 // TODO: engine.ReadEval()
@@ -26,108 +25,75 @@ func NewEngine() Engine {
 // TODO: engine.DefFunction()
 // TODO: engine.DefMacro()
 
+// TODO: handle multiline values
+
 // TODO: make prompt a function (of what?)
 
 // TODO: what do we export? Engine, Value
 
-// TODO: handle multiline values
+// TODO: decide if we need an interface? when do we create interfaces?
 
-func (e Engine) Repl(prompt string) {
-	env := e.env
-	reader := bufio.NewReader(os.Stdin)
-	// Direct mode = drop outer parens, and all on a single line.
-	direct := false
-	fmt.Println("Enter/exit direct mode with a single /")
-	for {
-		tempDirect := false
-		if direct {
-			fmt.Printf("%s>> ", prompt)
-		} else {
-			fmt.Printf("%s> ", prompt)
-		}
-		text, err := reader.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println()
-				bail()
-			}
-			fmt.Println("IO ERROR - ", err.Error())
-		}
-		text = strings.TrimSpace(text)
-		if text == "" {
-			continue
-		}
-		if text[0] == '/' {
-			if strings.TrimSpace(text[1:]) == "" {
-				direct = !direct
-				status := "off"
-				if direct {
-					status = "on"
-				}
-				fmt.Printf("Direct mode %s\n", status)
-				continue
-			} else {
-				text = text[1:]
-				tempDirect = true
-			}
-		}
-		var v Value
-		if direct || tempDirect {
-			v, _, err = readList(text)
-			if err != nil {
-				fmt.Println("READ ERROR -", err.Error())
-				continue
-			}
-		} else {
-			v, _, err = read(text)
-			if err != nil {
-				fmt.Println("READ ERROR -", err.Error())
-				continue
-			}
-		}
-		// check if it's a declaration
-		d, err := parseDef(v)
-		if err != nil {
-			fmt.Println("PARSE ERROR -", err.Error())
-			continue
-		}
-		if d != nil {
-			if d.typ == DEF_FUNCTION {
-				update(env, d.name, &vFunction{d.params, d.body, env})
-				fmt.Println(d.name)
-				continue
-			}
-			if d.typ == DEF_VALUE {
-				v, err := d.body.eval(env)
-				if err != nil {
-					fmt.Println("EVAL ERROR -", err.Error())
-					continue
-				}
-				update(env, d.name, v)
-				fmt.Println(d.name)
-				continue
-			}
-			fmt.Println("DECLARE ERROR - unknow declaration type", d.typ)
-			continue
-		}
-		// check if it's an expression
-		e, err := parseExpr(v)
-		if err != nil {
-			fmt.Println("PARSE ERROR -", err.Error())
-			continue
-		}
-		///fmt.Println("expr =", e.str())
-		v, err = e.eval(env)
-		if err != nil {
-			fmt.Println("EVAL ERROR -", err.Error())
-			continue
-		}
-		if !v.isNil() {
-			fmt.Println(v.Display())
-		}
-	}
+// TODO: figure out how to create documentation
+// TODO: exceptions? Simple catch / throw?
+// (string x)
+// (number y)
+// (bool z)
+
+// Put direct in the engine itself?
+
+func (e Engine) Read(line string, prefix string) (Value, error) {
+	text := prefix + " " + line
+	// TODO: distinguish error from "incomplete"?
+	v, _, err := read(text)
+	return v, err
 }
 
-func bail() {
-	os.Exit(0)
+func (e Engine) ReadList(line string) (Value, error) {
+	text := line
+	v, _, err := readList(text)
+	return v, err
+}
+
+func (e Engine) ProcessDeclaration(v Value) (string, error) {
+	// TODO: IsDeclaration that also returns the declaration.
+	// engine.Def(decl) operation
+	d, err := parseDef(v)
+	if err != nil {
+		return "", err
+	}
+	if d != nil {
+		if d.typ == DEF_FUNCTION {
+			update(e.env, d.name, &vFunction{d.params, d.body, e.env})
+			return d.name, nil
+		}
+		if d.typ == DEF_VALUE {
+			v, err := d.body.eval(e.env)
+			if err != nil {
+				fmt.Println("EVAL ERROR -", err.Error())
+			}
+			update(e.env, d.name, v)
+			return d.name, nil
+		}
+		return "", fmt.Errorf("DECLARE ERROR - unknow declaration type %s", d.typ)
+	}
+	return "", nil
+}
+
+func (e Engine) AddPrimitive(name string, min int, max int, p func(string, []Value) (Value, error)) {
+	pr := NewPrimitive(name, MakePrimitive(Primitive{name, min, max, p}))
+	update(e.env, name, pr)
+}
+
+func (e Engine) Eval(sexpr Value) (Value, error) {
+	expr, err := parseExpr(sexpr)
+	if err != nil {
+		fmt.Println()
+		return nil, fmt.Errorf("PARSE ERROR - %w", err.Error())
+	}
+	///fmt.Println("expr =", e.str())
+	v, err := expr.eval(e.env)
+	if err != nil {
+		return nil, fmt.Errorf("EVAL ERROR - %w", err.Error())
+	}
+	return v, nil
 }
